be5f11c02774f50f75ffcd84ffee2b30f0c68604
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-05 06:59:25 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyBWQu-H3fXG4C3djbd0YwmgIExnaxU8d3g",
+  authDomain: "kombat-c1f13.firebaseapp.com",
+  projectId: "kombat-c1f13",
+  storageBucket: "kombat-c1f13.appspot.com",
+  messagingSenderId: "819689626667",
+  appId: "1:819689626667:web:cd3e697d4b765805cf5e6e",
+  measurementId: "G-RSBHKS27GT"
 };
 
 const app = initializeApp(firebaseConfig);
