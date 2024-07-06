5785e0e41f71f36e802dc17cd1dc5ad47b5c1249
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-07 03:57:54 +0800

    Update TaskOne.js

--- src/Components/TaskOne.js
+++ src/Components/TaskOne.js
@@ -28,13 +28,12 @@ const TaskOne = ({ showModal, setShowModal }) => {
 
   const [isMissionButtonDisabled, setIsMissionButtonDisabled] = useState(true);
 
-
   useEffect(() => {
     const handleBackButtonClick = () => {
       setShowModal(false);
       document.getElementById("footermain").style.zIndex = "";
     };
-  
+
     if (showModal) {
       window.Telegram.WebApp.BackButton.show();
       window.Telegram.WebApp.BackButton.onClick(handleBackButtonClick);
@@ -42,15 +41,12 @@ const TaskOne = ({ showModal, setShowModal }) => {
       window.Telegram.WebApp.BackButton.hide();
       window.Telegram.WebApp.BackButton.offClick(handleBackButtonClick);
     }
-  
+
     // Cleanup handler when component unmounts
     return () => {
       window.Telegram.WebApp.BackButton.offClick(handleBackButtonClick);
     };
   }, [showModal, setShowModal]);
-  
-
-
 
   useEffect(() => {
     const telegramUserid = window.Telegram.WebApp.initDataUnsafe?.user?.id;
@@ -62,7 +58,6 @@ const TaskOne = ({ showModal, setShowModal }) => {
     if (telegramUserid) {
       fetchCountFromFirestore(telegramUserid).then((userCount) => {
         setCount(userCount);
-    
       });
 
       checkTaskCompletion(telegramUserid, taskID).then((completed) => {
@@ -75,8 +70,6 @@ const TaskOne = ({ showModal, setShowModal }) => {
     }
     // eslint-disable-next-line
   }, []);
-  
-
 
   const handleTaskLinkClick = () => {
     window.open("https://t.me/teteknibos");
@@ -150,8 +143,7 @@ const TaskOne = ({ showModal, setShowModal }) => {
     }
   };
 
-
-  const updateUserCountInFirestore = async (userid, newCount ) => {
+  const updateUserCountInFirestore = async (userid, newCount) => {
     try {
       const userRef = collection(db, "telegramUsers");
       const querySnapshot = await getDocs(userRef);
@@ -172,6 +164,30 @@ const TaskOne = ({ showModal, setShowModal }) => {
     } catch (e) {
       console.error("Error updating user count in Firestore: ", e);
     }
+  };
+
+  const saveTaskCompletionToFirestore = async (userid, taskId, status) => {
+    try {
+      const taskRef = doc(db, "taskCompletion", `${userid}_${taskId}`);
+      await setDoc(taskRef, { completed: status }, { merge: true });
+    } catch (e) {
+      console.error("Error saving task completion status: ", e);
+    }
+  };
+
+  const checkTaskCompletion = async (userid, taskId) => {
+    try {
+      const taskRef = doc(db, "taskCompletion", `${userid}_${taskId}`);
+      const taskDoc = await getDoc(taskRef);
+      return taskDoc.exists() && taskDoc.data().completed;
+    } catch (e) {
+      console.error("Error checking task completion status: ", e);
+      return false;
+    }
+  };
+
+  const handleComplete = (status) => {
+    setOpenComplete(status);
   };
 
   const finishMission = async () => {
@@ -199,7 +215,6 @@ const TaskOne = ({ showModal, setShowModal }) => {
     }
   };
 
-
   return (
     <>
       {showModal ? (
@@ -223,148 +238,63 @@ const TaskOne = ({ showModal, setShowModal }) => {
               <p className="w-full text-center text-[14px] font-semibold text-[#49ee49] pb-4">
                 {taskCompleted ? "Task is Completed" : ""}
               </p>
-              <div className="bg-cards rounded-[10px] p-[14px] flex justify-between items-center">
-                <div className="flex flex-1 items-center space-x-2">
-                  <div className="">
-                    <img src={coinsmall} className="w-[50px]" alt="Coin Icon" />
-                  </div>
-                  <div className="flex flex-col space-y-1">
-                    <span className="font-semibold">Reward</span>
-                    <div className="flex items-center">
-                      <span className="font-medium">50 000</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <h1 className="text-[20px] font-semibold pt-6 pb-4 px-2">
-                Your Tasks
-              </h1>
-              <div className="bg-cards rounded-[10px] p-[14px] flex justify-between items-center">
-                <div className="flex flex-1 items-center space-x-2">
-                  <div className="flex flex-col space-y-1">
-                    <span className="font-semibold">
-                      Join the Telegram Channel
-                    </span>
+              <button
+                className="transition-all duration-300 ease-in-out bg-[#4b64bc] text-[#fff] text-[16px] font-semibold py-3 px-6 rounded-[10px] w-full"
+                style={{ display: showTaskButton ? "block" : "none" }}
+                onClick={handleTaskLinkClick}
+              >
+                Join Telegram
+              </button>
 
-                    {message && (
-                      <span className="text-[#ea5b48] text-[12px] pr-8">
-                        {message}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="">
-                  {taskCompleted ? (
-                    <></>
-                  ) : (
-                    <>
-                      {showTaskButton && (
-                        <button
-                          onClick={handleTaskLinkClick}
-                          className={`flex font-medium bg-btn hover:bg-[#1e3356] ease-in duration-300 py-[6px] px-4 rounded-[8px] items-center justify-center text-[16px]`}
-                        >
-                          Go
-                        </button>
-                      )}
-                    </>
-                  )}
+              <button
+                className="transition-all duration-300 ease-in-out bg-[#4b64bc] text-[#fff] text-[16px] font-semibold py-3 px-6 rounded-[10px] w-full"
+                style={{ display: showCheckButton ? "block" : "none" }}
+                onClick={handleVerify}
+              >
+                Verify Task
+              </button>
 
-                  {showCheckButton && (
-                    <button
-                      onClick={handleVerify}
-                      className="flex font-medium bg-btn py-[6px] px-4 rounded-[8px] items-center justify-center text-[16px]"
-                    >
-                      <span> Check</span>
-                      <span className="text-[#b0b0b0] pointer-events-none select-none">
-                        {counter !== null && (
-                          <>
-                            <span className="text-[#fff]">ing</span>
-                            {` ${counter}s`}
-                          </>
-                        )}
-                      </span>
-                    </button>
-                  )}
-                  {showDoneButton && (
-                    <button
-                      id="done"
-                      className="text-[#7cf47c] font-medium py-[6px] px-4 rounded-[8px] items-center justify-center text-[16px]"
-                    >
-                      Done
-                    </button>
-                  )}
+              <button
+                className="transition-all duration-300 ease-in-out bg-[#4b64bc] text-[#fff] text-[16px] font-semibold py-3 px-6 rounded-[10px] w-full"
+                style={{ display: showDoneButton ? "block" : "none" }}
+                onClick={() => handleComplete(true)}
+              >
+                Complete
+              </button>
 
-                  {taskCompleted && <></>}
-                </div>
-              </div>
-              {taskCompleted ? (
-                <>
-                  <button
-                    className={`my-6 w-full py-5 px-3 flex items-center rounded-[12px] justify-center text-center text-[20px] font-medium text-[#6a6978] bg-btn2`}
-                  >
-                    Mission Completed
-                  </button>
-                </>
-              ) : (
-                <>
-                  <button
-                    onClick={() => handleComplete(true)}
-                    disabled={isMissionButtonDisabled}
-                    className={`my-6 w-full py-5 px-3 flex items-center rounded-[12px] justify-center text-center text-[20px] font-medium 
-                                    ${
-                                      isMissionButtonDisabled
-                                        ? "text-[#6a6978] bg-btn2"
-                                        : "text-[#f4f4f4] bg-btn"
-                                    }`}
-                  >
-                    Finish Mission
-                  </button>
-                </>
-              )}
-            </div>
-
-            <div
-              className={`${
-                openComplete === true ? "visible" : "invisible"
-              } absolute bottom-0 left-0 right-0 h-[76vh] bg-[#1e2340f7] z-[100] rounded-tl-[20px] rounded-tr-[20px] flex justify-center px-4 py-5`}
-            >
-              <div className="w-full flex flex-col justify-between py-8">
-                <div className="w-full flex justify-center flex-col items-center">
-                  <div className="w-[120px] h-[120px] rounded-[14px] bg-[#252e57] flex items-center justify-center">
-                    <img alt="claim" src={claim} className="" />
-                  </div>
-                  <h3 className="font-semibold text-[28px] py-4">
-                    Congratulations
-                  </h3>
-                  <p className="pb-6 text-[#9a96a6] text-[16px]">
-                    You have successfully completed the mission
-                  </p>
-
-                  <div className="flex flex-1 items-center space-x-2">
-                    <div className="">
-                      <img
-                        src={coinsmall}
-                        className="w-[25px]"
-                        alt="Coin Icon"
-                      />
-                    </div>
-                    <div className="font-bold text-[20px]">50 000</div>
-                  </div>
-                </div>
-
-                <div className="w-full flex justify-center pb-12">
-                  <button
-                    onClick={finishMission}
-                    className="bg-gradient-to-b gradient from-[#ffba4c] to-[#aa6900] w-full py-5 px-3 flex items-center justify-center text-center rounded-[12px] font-semibold text-[22px]"
-                  >
-                    Claim
-                  </button>
-                </div>
-              </div>
+              <p className="w-full text-center text-[14px] font-semibold text-[#e35050] pt-4">
+                {message}
+              </p>
             </div>
           </div>
         </div>
       ) : null}
+      {openComplete && (
+        <div className="fixed z-50 left-0 right-0 top-0 bottom-0 flex justify-center px-[16px] h-full completebg">
+          <div className="w-full flex flex-col items-center justify-center space-y-10">
+            <div className="text-center">
+              <img
+                src={coinsmall}
+                className="w-[150px] mx-auto mb-4 animate-bounce"
+                alt="coinsmall"
+              />
+              <h1 className="text-[26px] font-semibold pb-2 text-[#EAB543]">
+                +50,000
+              </h1>
+              <p className="text-[#9a96a6] text-[16px] font-medium">
+                Energy have been added to your balance.
+              </p>
+            </div>
+            <button
+              className="transition-all duration-300 ease-in-out bg-[#4b64bc] text-[#fff] text-[16px] font-semibold py-3 px-6 rounded-[10px] w-full"
+              onClick={finishMission}
+              disabled={isMissionButtonDisabled}
+            >
+              Finish Mission
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
