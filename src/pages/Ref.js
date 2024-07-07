205d448e12e12b3d02cc60ffc6f3b7046a219ac3
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-07 21:27:47 +0800

    Update Ref.js

--- src/pages/Ref.js
+++ src/pages/Ref.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 import Animate from "../Components/Animate";
 import { Outlet } from "react-router-dom";
 import ClaimLeveler from "../Components/ClaimLeveler";
@@ -18,10 +18,13 @@ const Ref = () => {
   const [loading, setLoading] = useState(true);
   const [copied, setCopied] = useState(false);
 
-  const formattedCount = new Intl.NumberFormat().format(count).replace(/,/g, " ");
+  const formattedCount = useMemo(() => {
+    return new Intl.NumberFormat().format(count).replace(/,/g, " ");
+  }, [count]);
 
   useEffect(() => {
-    const telegramUsername = window.Telegram.WebApp.initDataUnsafe?.user?.username;
+    const telegramUsername =
+      window.Telegram.WebApp.initDataUnsafe?.user?.username;
     const telegramUserid = window.Telegram.WebApp.initDataUnsafe?.user?.id;
 
     if (telegramUsername) {
@@ -31,10 +34,10 @@ const Ref = () => {
       setIdme(telegramUserid);
     }
 
-    fetchAllUsers();
+    fetchAllUsers(); // Fetch all users when the component mounts
   }, []);
 
-  const fetchAllUsers = async () => {
+  const fetchAllUsers = useCallback(async () => {
     try {
       const userRef = collection(db, "telegramUsers");
       const querySnapshot = await getDocs(userRef);
@@ -43,7 +46,10 @@ const Ref = () => {
 
       querySnapshot.forEach((doc) => {
         const data = doc.data();
-        const { username, fullname, refereeId, count } = data;
+        const username = data.username;
+        const fullname = data.fullname;
+        const refereeId = data.refereeId;
+        const count = data.count;
 
         if (!uniqueUsernames.has(username)) {
           allUsers.push({ username, fullname, refereeId, count });
@@ -52,15 +58,14 @@ const Ref = () => {
       });
 
       setUsers(allUsers);
-      setFilteredUsers(allUsers.filter(user => user.refereeId === idme));
-      setLoading(false);
+      setLoading(false); // Set loading to false once data is fetched
     } catch (error) {
       console.error("Error fetching users: ", error);
       setLoading(false);
     }
-  };
+  }, []);
 
-  const copyToClipboard = () => {
+  const copyToClipboard = useCallback(() => {
     const telegramUserid = window.Telegram.WebApp.initDataUnsafe?.user?.id;
 
     if (telegramUserid) {
@@ -75,7 +80,9 @@ const Ref = () => {
           setCopied(true);
           setTimeout(() => setCopied(false), 10000);
         })
-        .catch(err => console.error('Failed to copy text: ', err));
+        .catch(err => {
+          console.error('Failed to copy text: ', err);
+        });
     } else {
       const textArea = document.createElement('textarea');
       textArea.value = reflink;
@@ -90,28 +97,7 @@ const Ref = () => {
       }
       document.body.removeChild(textArea);
     }
-  };
-
-  const ReferralCard = ({ user }) => (
-    <div className="bg-cards rounded-[10px] p-[14px] flex flex-wrap justify-between items-center">
-      <div className="flex flex-1 flex-col space-y-1">
-        <div className="text-[#fff] pl-1 text-[16px] font-semibold">
-          {user.fullname}
-        </div>
-        <div className="flex items-center space-x-1 text-[14px] text-[#e5e5e5]">
-          <img src={bronze} alt="bronze" className="w-[18px]" />
-          <span className="font-medium text-[#9a96a6]">Bronze</span>
-          <span className="bg-[#bdbdbd] w-[1px] h-[13px] mx-2"></span>
-          <img src={coinsmall} className="w-[20px]" alt="coin" />
-          <span className="font-normal text-[#ffffff] text-[15px]">{user.count}</span>
-        </div>
-      </div>
-      <div className="text-[#ffce68] font-semibold text-[14px]">+0</div>
-      <div className="flex w-full mt-2 p-[4px] items-center bg-energybar rounded-[10px] border-[1px] border-borders">
-        <div className="h-[10px] rounded-[8px] bg-btn w-[.5%]"></div>
-      </div>
-    </div>
-  );
+  }, []);
 
   return (
     <>
@@ -126,33 +112,13 @@ const Ref = () => {
               </h1>
               <span className="text-[#6ed86e] font-semibold text-[16px]"></span>
             </div>
-            <div className="w-full bg-cards rounded-[12px] px-3 py-4 flex flex-col">
-              <span className="w-full flex justify-between items-center pb-2">
-                <h2 className="text-[18px] font-semibold">My invite link:</h2>
-                <span
-                  onClick={copyToClipboard}
-                  className="bg-gradient-to-b from-[#094e9d] to-[#0b62c4] font-medium py-[6px] px-4 rounded-[12px] flex items-center justify-center text-[16px]"
-                >
-                  {copied ? <span>Copied!</span> : <span>Copy</span>}
-                </span>
-              </span>
-              <div className="text-[#9a96a6] text-[13px]">
-                https://t.me/Tetekdf_bot?start=r{idme}
-              </div>
-            </div>
+            <InviteLink
+              idme={idme}
+              copied={copied}
+              copyToClipboard={copyToClipboard}
+            />
             <div className="bg-borders w-full px-5 h-[1px] !mt-6"></div>
-            <div className="w-full flex flex-col">
-              <h3 className="text-[22px] font-semibold pb-[16px]">My Referrals:</h3>
-              <div className="w-full flex flex-col space-y-3">
-                {filteredUsers.length > 0 ? (
-                  filteredUsers.map((user, index) => <ReferralCard key={index} user={user} />)
-                ) : (
-                  <p className="w-full text-center text-[16px] py-12 font-medium">
-                    You don't have referrals😭
-                  </p>
-                )}
-              </div>
-            </div>
+            <ReferralList filteredUsers={filteredUsers} />
             <ClaimLeveler claimLevel={claimLevel} setClaimLevel={setClaimLevel} />
           </div>
           <Outlet />
@@ -161,5 +127,65 @@ const Ref = () => {
     </>
   );
 };
+
+const InviteLink = ({ idme, copied, copyToClipboard }) => (
+  <div className="w-full bg-cards rounded-[12px] px-3 py-4 flex flex-col">
+    <span className="w-full flex justify-between items-center pb-2">
+      <h2 className="text-[18px] font-semibold">My invite link:</h2>
+      <span
+        onClick={copyToClipboard}
+        className="bg-gradient-to-b from-[#094e9d] to-[#0b62c4] font-medium py-[6px] px-4 rounded-[12px] flex items-center justify-center text-[16px]"
+      >
+        {copied ? <span>Copied!</span> : <span>Copy</span>}
+      </span>
+    </span>
+    <div className="text-[#9a96a6] text-[13px]">
+      https://t.me/Tetekdf_bot?start=r{idme}
+    </div>
+  </div>
+);
+
+const ReferralList = ({ filteredUsers }) => (
+  <div className="w-full flex flex-col">
+    <h3 className="text-[22px] font-semibold pb-[16px]">My Referrals:</h3>
+    <div className="w-full flex flex-col space-y-3">
+      {filteredUsers.length > 0 ? (
+        filteredUsers.map((user, index) => (
+          <div
+            key={index}
+            className="bg-cards rounded-[10px] p-[14px] flex flex-wrap justify-between items-center"
+          >
+            <div className="flex flex-1 flex-col space-y-1">
+              <div className="text-[#fff] pl-1 text-[16px] font-semibold">
+                {user.fullname}
+              </div>
+              <div className="flex items-center space-x-1 text-[14px] text-[#e5e5e5]">
+                <div className="">
+                  <img src={bronze} alt="bronze" className="w-[18px]" />
+                </div>
+                <span className="font-medium text-[#9a96a6]">Bronze</span>
+                <span className="bg-[#bdbdbd] w-[1px] h-[13px] mx-2"></span>
+                <span className="w-[20px]">
+                  <img src={coinsmall} className="w-full" alt="coin" />
+                </span>
+                <span className="font-normal text-[#ffffff] text-[15px]">
+                  {user.count}
+                </span>
+              </div>
+            </div>
+            <div className="text-[#ffce68] font-semibold text-[14px]">+0</div>
+            <div className="flex w-full mt-2 p-[4px] items-center bg-energybar rounded-[10px] border-[1px] border-borders">
+              <div className="h-[10px] rounded-[8px] bg-btn w-[.5%]"></div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className="w-full text-center text-[16px] py-12 font-medium">
+          You don't have referrals😭
+        </p>
+      )}
+    </div>
+  </div>
+);
 
 export default Ref;
