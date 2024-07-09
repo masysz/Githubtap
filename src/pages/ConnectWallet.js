f02345e13ad46bdd851ffea7723781b08bbdf909
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-10 02:37:28 +0800

    Update ConnectWallet.js

--- src/pages/ConnectWallet.js
+++ src/pages/ConnectWallet.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+import "../App.css";
 import Animate from "./Components/Animate";
 import Spinner from "./Components/Spinner";
 import { TonConnectButton } from "@tonconnect/ui-react";
