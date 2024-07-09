69781a660151b7933a774f0ac1cdba4885972b4b
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-10 02:59:41 +0800

    Update ConnectWallet.js

--- src/pages/ConnectWallet.js
+++ src/pages/ConnectWallet.js
@@ -5,24 +5,19 @@ import Spinner from "../Components/Spinner";
 import { TonConnectButton } from "@tonconnect/ui-react";
 
 const Connect = () => {
-  const [loading, setLoading] = useState(false);
+
 
   return (
     <>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Animate>
+
           <div className="w-full justify-center flex-col space-y-3 px-5">
             <div className="fixed top-0 left-0 right-0 pt-8 px-5">
               <div className="w-full items-center justify-center pb-3 flex pt-2">
                 <TonConnectButton />
               </div>
             </div>
+            </div>
             <Outlet />
-          </div>
-        </Animate>
-      )}
     </>
   );
 };
