e20962541dff32f1ed5e1305c9c9287123d1dd04
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-10 19:10:51 +0800

    update

--- src/pages/TapHome.js
+++ src/pages/TapHome.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef, useContext } from "react";
 import styled, { keyframes } from "styled-components";
 import "../App.css";
 import coinsmall from "../images/coinsmall.webp";
-import tapmecoin from "../images/tapmem.png";
+import tapmecoin from "../images/coinsmall.webp";
 import bronze from "../images/bronze.webp";
 import silver from "../images/silver.webp";
 import gold from "../images/gold.webp";
