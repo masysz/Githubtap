69ae9445d66548b9f1ad3540d61bf33bbf905112
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-10 08:27:45 +0800

    Update Levels.js

--- src/Components/Levels.js
+++ src/Components/Levels.js
@@ -81,7 +81,7 @@ const Levels = ({ showLevels, setShowLevels }) => {
             for (let i = 0; i < levels.length; i++) {
                 const levelName = levels[i].name;
                 const userRef = collection(db, 'telegramUsers');
-                const q = query(userRef, where('level', '==', levelName), orderBy('count', 'desc'));
+                const q = query(userRef, where('level', '==', levelName));
 
                 try {
                     const querySnapshot = await getDocs(q);
