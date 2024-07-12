27d2b08775c3a685247ae1ed9cf46751fba849f0
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-13 04:04:21 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,9 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore(app);
 console.log("Firebase initialized:", app);
 console.log("Firestore initialized:", db);
 
-export { db, auth };
+export { db };
