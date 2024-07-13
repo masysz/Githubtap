bfec1bc92485614d39225e80c8df670c448d0dc7
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-13 16:34:40 +0800

    Update userContext.js

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -16,7 +16,7 @@ export const UserProvider = ({ children }) => {
   const [balance, setBalance] = useState(0);
   // const [totalBalance, setTotalBalance] = useState(0);
   const [tapBalance, setTapBalance] = useState(0);
-  const [level, setLevel] = useState({ id: 1, name: "Bronze", imgUrl: '/bronze.webp' }); // Initial level as an object with id and name
+  const [level, setLevel] = useState({ id: 1, name: "Bronze", imgUrl: '/babyfire.png' }); // Initial level as an object with id and name
   const [tapValue, setTapValue] = useState({level: 1, value: 1});
   const [timeRefill, setTimeRefill] = useState({level: 1, duration: 10, step: 600});
   const [id, setId] = useState("");
