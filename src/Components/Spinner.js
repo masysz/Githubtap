3791d11255eb5b667958061d385a0799e29a5969
redcrexio <168323806+wormsolgame@users.noreply.github.com>
2024-07-17 00:07:32 +0800

    update

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className="spinner">
-    <div className="circle"></div>
-  </div>
+<div className="spinner">
+<div className="spin1 animate-spin">
+    <div className="spin2">
+
+</div>
+    <div className="spin3">
+
+</div><div className="spin4">
+
+</div><div className="spin5">
+
+</div>
+
+<div className="spin6">
+
+</div><div className="spin7">
+
+</div><div className="spin8">
+
+</div><div className="spin9">
+
+</div></div>
+    
+</div>
   )
 }
 
