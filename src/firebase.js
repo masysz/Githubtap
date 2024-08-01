515ff40fc9af98431755cc1138fde51845fd6a86
masysz <67843831+masysz@users.noreply.github.com>
2024-08-01 09:13:25 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXgBykiefZ5oPv8KsAEbrJrFXJNVzJ9Fo",
-  authDomain: "apptelebot.firebaseapp.com",
-  projectId: "apptelebot",
-  storageBucket: "apptelebot.appspot.com",
-  messagingSenderId: "464223544142",
-  appId: "1:464223544142:web:c3fdb64c21d75439f1f41f"
+  apiKey: "AIzaSyCssFBeOEa2JHUbxM3IAaZv9K3c3bktXfA",
+  authDomain: "apecomtest.firebaseapp.com",
+  projectId: "apecomtest",
+  storageBucket: "apecomtest.appspot.com",
+  messagingSenderId: "613346686609",
+  appId: "1:613346686609:web:c8e9e8bbd56edf0c403130"
 
 };
 
