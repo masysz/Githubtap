a321baa50c99f2589523ed5cc95f79f67a8566c6
masysz <67843831+masysz@users.noreply.github.com>
2024-08-01 19:27:43 +0700

    Update Ref.js

--- src/pages/Ref.js
+++ src/pages/Ref.js
@@ -16,7 +16,7 @@ const Ref = () => {
  
   const copyToClipboard = () => {
 
-   const reflink = `https://t.me/Apecommercetestbot/apecom?startapp=r${id}
+   const reflink = `https://t.me/Apecommercetestbot?start=r${id}
 Play with me, and get free token airdrop! 
 Tap Geto for more coin and invite friends… 
 More friends more coin!!
@@ -90,7 +90,7 @@ More friends more coin!!
               </span>
             </span>
             <div className="text-[#9a96a6] text-[13px]">
-            https://t.me/Apecommercetestbot/apecom?startapp=r{id}
+            https://t.me/Apecommercetestbot?start=r{id}
             </div>
           </div>
           <div className="bg-borders w-full px-5 h-[1px] !mt-6"></div>
