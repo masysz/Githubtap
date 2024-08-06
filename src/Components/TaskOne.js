330b597b09a86d3ebba6b8323bcd2e115c4dcef8
masysz <67843831+masysz@users.noreply.github.com>
2024-08-06 11:04:16 +0700

    Update TaskOne.js

--- src/Components/TaskOne.js
+++ src/Components/TaskOne.js
@@ -85,7 +85,7 @@ const TaskOne = ({ showModal, setShowModal }) => {
     }
 
     const response = await fetch(
-      `https://api.telegram.org/bot7436444125:AAGB8IwBNOvRbpW-AjR0HPvMOC0qGSTnILU/getChatMember?chat_id=@geto_spirit_announcement&user_id=${id}`
+      `https://api.telegram.org/bot7436444125:AAGB8IwBNOvRbpW-AjR0HPvMOC0qGSTnILU/getChatMember?chat_id=@jurnaliscryptochannel&user_id=${id}`
     );
     const data = await response.json();
 
